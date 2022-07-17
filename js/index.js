d9e9271cf7388746d454a6b164b8037ee451e761
Samu Ross <safero99@gotmail.com>
2022-07-16 20:21:15 -0500

    agregados los cambios que dijo carolina

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
 window.sr = ScrollReveal();
- sr.reveal('')
+
+    sr.reveal('.index--seMas', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-100px',
+        delay: 1000
+    });
+    sr.reveal('.if-you-are', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '300px',
+        delay: 3000
+    });
+
+   
