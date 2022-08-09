30823236eab30de424f156eb510353fb779b528b
Samu Ross <safero99@gotmail.com>
2022-08-09 18:25:16 -0500

    Finalizada la seccion de serivicios

---
+++ js/servicios.js
@@ -0,0 +1,30 @@
+window.sr = ScrollReveal();
+    // header 
+    sr.reveal('.header', {
+        duration: 3000,
+        origin: 'top',
+        distance: '100px',
+    });
+    // menu navegacion
+    sr.reveal('.menu-navegacion', {
+        duration: 3000,
+        origin: 'left',
+        distance: '100px',
+    });
+    sr.reveal('.tarjeta', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '100px'
+    })
+    sr.reveal('.servicios--description', {
+        duration: 3000,
+        origin: 'right',
+        distance: '100px'
+    })
+    sr.reveal('#footer-servicios', {
+        duration: 3000,
+        origin: 'left',
+        distance: '100px'
+    })
+    
+    
